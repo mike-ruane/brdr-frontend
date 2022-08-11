b2a823ab81ac82717f1f46b97f1f0e6005aac030
Mike Ruane <mike.jruane@gmail.com>
2022-08-11 22:28:09 +0100

    lean navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,20 +5,20 @@
 </script>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-	<div class="container-fluid">
-		<a href="/" class="navbar-brand" class:active={$page.url.pathname === '/'}>Home</a>
+	<div class="container-fluid" >
+		<span class="navbar-brand">{username}</span>
 		<div>
 			{#if username}
-				<ul class="navbar-nav me-auto mb-2 mb-md-0">
-					<li class="nav-item">
-						<a href="/about" class="nav-link" class:active={$page.url.pathname === '/about'}
-							>About</a
-						>
-					</li>
-					<li class="nav-item">
-						<a href="/logout" class="nav-link">Logout</a>
-					</li>
-				</ul>
+				<div class="dropdown" >
+					<button class="btn btn-dark" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+						<i class="bi bi-list"></i>
+					</button>
+					<ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+						<li><a href="/" class="dropdown-item" class:active={$page.url.pathname === '/'}>Home</a></li>
+						<li><a href="/about" class="dropdown-item" class:active={$page.url.pathname === '/about'}>About</a></li>
+						<li><a href="/logout" class="dropdown-item">Logout</a></li>
+					</ul>
+				</div>
 			{:else}
 				<ul class="navbar-nav me-auto mb-2 mb-md-0">
 					<li class="nav-item">
