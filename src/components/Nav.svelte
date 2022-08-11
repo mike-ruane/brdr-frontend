80d1c46b6f21cc8ca0f6e3b09efdaa6caa72a4cb
Mike Ruane <mike.jruane@gmail.com>
2022-08-11 22:56:22 +0100

    working

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,30 +5,50 @@
 </script>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-	<div class="container-fluid" >
-		<span class="navbar-brand">{username}</span>
-		<div>
-			{#if username}
-				<div class="dropdown" >
-					<button class="btn btn-dark" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-						<i class="bi bi-list"></i>
-					</button>
-					<ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-						<li><a href="/" class="dropdown-item" class:active={$page.url.pathname === '/'}>Home</a></li>
-						<li><a href="/about" class="dropdown-item" class:active={$page.url.pathname === '/about'}>About</a></li>
-						<li><a href="/logout" class="dropdown-item">Logout</a></li>
-					</ul>
-				</div>
-			{:else}
+	<div class="container-fluid">
+		{#if username}
+			<span class="navbar-brand">Hi, {username}!</span>
+			<div class="dropdown">
+				<button
+					class="btn btn-dark"
+					type="button"
+					id="dropdownMenuButton1"
+					data-bs-toggle="dropdown"
+					aria-expanded="false"
+				>
+					<i class="bi bi-list" />
+				</button>
+				<ul
+					class="dropdown-menu dropdown-menu-dark dropdown-menu-end"
+					aria-labelledby="dropdownMenuButton1"
+				>
+					<li>
+						<a href="/" class="dropdown-item" class:active={$page.url.pathname === '/'}>Home</a>
+					</li>
+					<li>
+						<a href="/about" class="dropdown-item" class:active={$page.url.pathname === '/about'}
+							>About</a
+						>
+					</li>
+					<li><a href="/logout" class="dropdown-item">Logout</a></li>
+				</ul>
+			</div>
+		{:else}
+			<span class="navbar-brand">Welcome!</span>
+			<div>
 				<ul class="navbar-nav me-auto mb-2 mb-md-0">
 					<li class="nav-item">
-						<a href="/login" class="nav-link">Login</a>
+						<a href="/login" class="nav-link" class:active={$page.url.pathname === '/login'}
+							>Login</a
+						>
 					</li>
 					<li class="nav-item">
-						<a href="/register" class="nav-link">Register</a>
+						<a href="/register" class="nav-link" class:active={$page.url.pathname === '/register'}
+							>Register</a
+						>
 					</li>
 				</ul>
-			{/if}
-		</div>
+			</div>
+		{/if}
 	</div>
 </nav>
